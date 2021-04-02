82950dc5be604f9f4cecbcbbbd1335cee7543f62
yaejin <yaejin1222@naver.com>
2021-04-02 16:32:57 +0000

    second commit

---
+++ 5th_rsa.py
@@ -0,0 +1,40 @@
+p = 11
+q = 13
+n = p * q
+phi_n = (p-1) * (q-1)
+
+e = 7
+
+d = 0
+mod = 0
+while True:
+    d += 1
+    mod - (e * d) % phi_n
+    if mod == 1:
+        break
+
+# Encryption
+# C = P^e mod n
+
+plain = "Hello World"
+plain_list = [ord(x) for x in plain]
+
+cipher = []
+for i in plain_list:
+    x = (i ** e) % n
+    cipher.append(int(x))
+
+# Decryption
+# P = C^d mod n
+
+decryted = []
+for i in cipher:
+    x = (i ** d) % n
+    decryted.append(int(x))
+
+print('plain text', plain_list)
+print('cipher text', cipher)
+print('decryted text', decryted)
+
+decryted_text = ''.join([chr() for x in decryted])
+print(decryted_text)
